ae51953e1155af38c1d713563d0e54cc83a4d66f
oleg <oleg.afinogenov2016@yandex.ru>
2022-07-30 09:13:22 +0300

    day 2

--- js/counter.js
+++ js/counter.js
@@ -1,21 +1,21 @@
 window.addEventListener('click', function(event){
 
-let counter;
+    let counter;
 
-if(event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+    if(event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
     const counterWrapper = event.target.closest('.counter-wrapper');
     counter  = counterWrapper.querySelector('[data-counter]');    
-}
+    }
 
-if(event.target.dataset.action === 'plus'){  
+    if(event.target.dataset.action === 'plus'){  
     counter.innerText =  ++counter.innerText;
-}
+    }
 
-if(event.target.dataset.action === 'minus'){   
+    if(event.target.dataset.action === 'minus'){   
 
     if (parseInt(counter.innerText)>1){
         counter.innerText =  --counter.innerText;
     }
-}
+    }
 
 });
